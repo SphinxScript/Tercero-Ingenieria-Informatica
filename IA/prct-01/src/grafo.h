2fb11ad3b0c252cdd54672a8741b583fd69d0c6b
David Pane <alu0101643137@ull.edu.es>
2025-09-25 17:15:35 +0000

    ElementolistaAdyacencia cambia identificador como ElementoAdyacencia - ElementoAdyacencia terminado y Arista terminado - class Grafo empezada

--- IA/prct-01/src/grafo.h
+++ IA/prct-01/src/grafo.h
@@ -2,12 +2,16 @@
 #define GRAFO_H
 
 
-// este struct se utilizará para almacenar la información de un nodo. Un elemento de la lista de adyacencia, que es lo que usaré para saber sucesores, etc.
-// Posteriormente usaré esta lista y haré un vector de 
-struct ElementolistaAdyacencia {
+/*
+  este struct se utilizará para almacenar la información de un nodo. Un elemento de la lista de adyacencia, que es lo que usaré para saber sucesores, etc.
+  se utilizará este struct para almacenarlo posteriormente en un vector, donde dicho vector almacena un conjunto de ElementoAdyacencia
+  posteriormente otro vector almacena ese vector ElementoAdyacencia> (vector<vector<ElementoAdyacencia>>) y este vector se usará para
+  emplear como índice un nodo, y pues dicho nodo tiene un conjunto de adyacentes, con su coste (el de ElementoAdyacencia).
+*/
+struct ElementoAdyacencia {
   unsigned nodo;
   int coste;
-  ElementolistaAdyacencia(unsigned nodo_param, int coste_param = 1)
+ ElementoAdyacencia(unsigned nodo_param, int coste_param = 1)
   : nodo{nodo_param}, coste{coste_param} {}
 };
 
@@ -18,6 +22,14 @@ struct Arista {
   int coste;
   Arista(unsigned arista1, unsigned arista2)
   : extremo1{arista1}, extremo2{arista2} {}
+};
+
+class Grafo {
+  public:
+
+  private:
+
+
 };
 
 #endif
