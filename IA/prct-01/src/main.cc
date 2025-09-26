dd74d3e6ef8b182f7ee830fbf7c6de7f77528432
David Pane <alu0101643137@ull.edu.es>
2025-09-26 09:33:14 +0000

    Main comenzado, para comprobar si se crea bien la lista de adyacencia

---
+++ IA/prct-01/src/main.cc
@@ -0,0 +1,14 @@
+#include "grafo.h"
+
+
+int main(int argc, char* argv[]) {
+  std::string nombrefichero{argv[1]};
+  if (argc != 2) {
+    std::cerr << "Error: se han pasado un número de argumentos no válido" << std::endl;
+    return 1;
+  }
+  int codigo_error;
+  Grafo objeto_grafo(nombrefichero, codigo_error);
+  
+  return 0;
+}
