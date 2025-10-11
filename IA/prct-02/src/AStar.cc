029ba1e1b6ffc6bd3f4d81b5d8a15793d2770200
David Pane <alu0101643137@ull.edu.es>
2025-10-11 12:11:19 +0000

    Starting A* search

--- IA/prct-02/src/AStar.cc
+++ IA/prct-02/src/AStar.cc
@@ -3,5 +3,17 @@
 
 AStar::AStar(Laberinto* laberinto) {
   laberinto_ = laberinto;
-  
+  // Extraemos el nodo del que partimos para la heuristica y creamos un objeto Nodo:
+  Coordenada coordenada_nodo = laberinto_->ObtenerInicio();
+  Nodo nodo_inicio;
+  nodo_inicio.posicion = coordenada_nodo;
+  nodo_inicio.coste = 0;
+  nodo_inicio.heuristica = CalculaHeuristica(coordenada_nodo, laberinto_->ObtenerFin());
+  nodo_inicio.total = nodo_inicio.coste + nodo_inicio.heuristica;
+  nodo_inicio.padre = {-1, -1}; // No tiene padre porque es el nodo inicial
+}
+
+int AStar::CalculaHeuristica(const Coordenada& coord_actual, const Coordenada& coord_final) const {
+  // Usamos la distancia Manhattan * 3 como heurística
+  return (abs(coord_final.fila - coord_actual.fila) + abs(coord_final.columna - coord_actual.columna)) * 3;
 }
--- IA/prct-02/src/AStar.h
+++ IA/prct-02/src/AStar.h
@@ -23,9 +23,13 @@ class AStar {
   const std::vector<Coordenada>& GetCamino() const { return camino_; }
  private:
   int CalculaHeuristica(const Coordenada& coord_actual, const Coordenada& coord_final) const;
+  int CalculaF(const Coordenada& coord_actual, const Coordenada& coord_final) const;
+  int CalculaG(const Coordenada& coord_actual, const Coordenada& coord_vecino) const;
   void ReconstruirCamino(const Coordenada& coord_final);
   Laberinto* laberinto_;
   std::vector<Coordenada> camino_;
+  std::vector<Nodo> nodos_abiertos_;
+  std::vector<Nodo> nodos_cerrados_;
 };
 
 #endif
